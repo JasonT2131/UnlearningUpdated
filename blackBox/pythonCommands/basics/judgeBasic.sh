bb019054095ca5778baf73b852a25d3838658fdb
JasonT2131 <jasontandiary@gmail.com>
2026-05-16 19:51:33 -0400

    Delete blackBox/pythonCommands/basics/judgeBasic.sh

--- blackBox/pythonCommands/basics/judgeBasic.sh
+++
@@ -1,5 +0,0 @@
-python judge.py --model 8B --forget 10 --hint 0 --mode basic
-python judge.py --model 8B --forget 05 --hint 0 --mode basic
-python judge.py --model 8B --forget 01 --hint 0 --mode basic
-python judge.py --model learnt --forget None --hint 0 --mode basic
-python judge.py --model neverLearnt --forget None --hint None --mode basic
