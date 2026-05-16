f78a2ef7353e378aafed07dda1f0a8f393cc0eac
JasonT2131 <jasontandiary@gmail.com>
2026-05-16 19:52:22 -0400

    Delete blackBox/pythonCommands/blanks/neverLearnt.sh

--- blackBox/pythonCommands/blanks/neverLearnt.sh
+++
@@ -1,4 +0,0 @@
-python quizNoHint.py --modelParam tofuNone --forget None
-python quizTaker.py --modelParam tofuNone --forget None --hints 0
-python quizTaker.py --modelParam tofuNone --forget None --hints 1
-python quizTaker.py --modelParam tofuNone --forget None --hints 2
