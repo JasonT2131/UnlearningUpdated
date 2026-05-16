3a5452635ee59ab2d54b49db7a60bf37eeafa82c
JasonT2131 <jasontandiary@gmail.com>
2026-05-16 19:52:58 -0400

    Delete blackBox/pythonCommands/paraphrase/paraphraseAll.sh

--- blackBox/pythonCommands/paraphrase/paraphraseAll.sh
+++
@@ -1,6 +0,0 @@
-
-python paraphraseAns.py --modelParam learnt --forget None
-python paraphraseAns.py --modelParam neverLearnt --forget None
-python paraphraseAns.py --model 8B --forget 01
-python paraphraseAns.py --model 8B --forget 05 
-python paraphraseAns.py --model 8B --forget 10 
