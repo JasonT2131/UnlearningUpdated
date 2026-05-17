2d6dab39c5cca4448bd152315a04c4b0f4c91ca0
JasonT2131 <jasontandiary@gmail.com>
2026-05-16 20:08:15 -0400

    Update quizTaker.py

--- blackBox/pythonCommands/blanks/quizTaker.py
+++ blackBox/pythonCommands/blanks/quizTaker.py
@@ -41,11 +41,11 @@ def cli(modelparam, forget, hints):
             modelName = "Llama3.1-8B"
 
 
-        tokenizer = AutoTokenizer.from_pretrained(f"/shared/share_mala/jt3585/newModels/{modelName}-forget{forget}-NPO")
+        tokenizer = AutoTokenizer.from_pretrained(#Unlearnt model path)
 
         tokenizer.pad_token = tokenizer.eos_token
 
-        model = AutoModelForCausalLM.from_pretrained(f"/shared/share_mala/jt3585/newModels/{modelName}-forget{forget}-NPO", device_map ="auto")
+        model = AutoModelForCausalLM.from_pretrained(#Unlearnt model path)
 
     base_prompt = """
 You must replace every instance of `--` with **exactly one word** so that your final answer contains no `--`. Each `--` represents **one missing word only**.
@@ -69,13 +69,13 @@ You will be given:
     hintsList = []
 
  
-    df = pd.read_csv("/user/jt3585/unlearn/blackBox/queries/blanks/blanksHints.csv", usecols= [f"Hint{hints}"])
+    df = pd.read_csv(#Path to hints, usecols= [f"Hint{hints}"])
     for i in df[f"Hint{hints}"]:
         hintsList.append(i)
 
 
-    output_file = f"/user/jt3585/unlearn/blackBox/answerGen/blankAnswers/{modelName}/filled/forget{forget}/Hint{hints}.txt"
-    blanks_file = "/user/jt3585/unlearn/blackBox/queries/blanks/blanks.txt"
+    output_file = #Output model Path
+    blanks_file = #Blank Quiz Path
     os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
 
